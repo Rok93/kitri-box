a033282d2067b7cebfa12b71d33650dbc9163b45
김경록 <goodboy3421@gmail.com>
2019-09-24 00:32:07 +0900

    LogoutServlet.java 파일 생성
    로그아웃의 Controller

---
+++ src/movie/LogoutServlet.java
@@ -0,0 +1,17 @@
+package movie;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        request.setCharacterEncoding("utf-8");
+
+
+    }
+
+}
