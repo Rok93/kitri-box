f7a3da10bc1ce9b7613aeacb6d6600631db826d5
김경록 <goodboy3421@gmail.com>
2019-09-24 00:29:55 +0900

    BookVO.java
    예매의 정보를 담는 Model(VO)

---
+++ src/movie/BookVO.java
@@ -0,0 +1,49 @@
+package movie;
+
+public class BookVO {
+	int bookid;
+	String userid;
+	int movieid;
+	String time;
+	int person;
+	String seat;
+	
+	public String getSeat() {
+		return seat;
+	}
+	public void setSeat(String seat) {
+		this.seat = seat;
+	}
+	public int getBookid() {
+		return bookid;
+	}
+	public void setBookid(int bookid) {
+		this.bookid = bookid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getPerson() {
+		return person;
+	}
+	public void setPerson(int person) {
+		this.person = person;
+	}
+	public int getMovieid() {
+		return movieid;
+	}
+	public void setMovieid(int movieid) {
+		this.movieid = movieid;
+	}
+	
+	
+}
