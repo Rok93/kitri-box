7accac9652340136d632f9bdfd578dedd32cb900
김경록 <goodboy3421@gmail.com>
2019-09-24 00:27:00 +0900

    MovieVO.java
    영화정보를 담는 Model(VO)

---
+++ src/movie/MovieVO.java
@@ -0,0 +1,93 @@
+package movie;
+
+public class MovieVO {
+	int movieid;
+	int book_count;
+	int runtime;
+	int audience_num;
+	int age_limit;
+	String poster;
+	String star;
+	String genre;
+	String title;
+	String startdate;
+	int during;
+	int score;
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getMovieid() {
+		return movieid;
+	}
+	public void setMovieid(int movieid) {
+		this.movieid = movieid;
+	}
+	public int getBook_count() {
+		return book_count;
+	}
+	public void setBook_count(int book_count) {
+		this.book_count = book_count;
+	}
+	public int getRuntime() {
+		return runtime;
+	}
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+	public int getAudience_num() {
+		return audience_num;
+	}
+	public void setAudience_num(int audience_num) {
+		this.audience_num = audience_num;
+	}
+	public int getAge_limit() {
+		return age_limit;
+	}
+	public void setAge_limit(int age_limit) {
+		this.age_limit = age_limit;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getStar() {
+		return star;
+	}
+	public void setStar(String star) {
+		this.star = star;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getStartdate() {
+		return startdate;
+	}
+	public void setStartdate(String startdate) {
+		this.startdate = startdate;
+	}
+	public int getDuring() {
+		return during;
+	}
+	public void setDuring(int during) {
+		this.during = during;
+	}
+	
+	
+}
